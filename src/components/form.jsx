b6da95410b6e69f13f204aa9c8bb5cfe9a062599
Benjamin Godø Mulelid <benjamin.mulelid@hotmail.no>
2024-02-20 16:59:52 +0100

    add firebase

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,6 @@
-import '../index.css';
-
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../utils/firebase';
 import Slider from './slider';
 
 const submit = () => {
@@ -11,10 +11,24 @@ const submit = () => {
  * Main page content. 
  */
 const Form = () => {
+    const [question, setQuestion] = useState('');
     const [value, setValue] = useState(51);
+
+    const fetchQuestion = async () => {
+        const docRef = doc(db, "questions", "1");
+        const docSnap = await getDoc(docRef);
+
+        console.log(docSnap.data().question);
+        setQuestion(docSnap.data().question);
+    }
+
+    useEffect(() => {
+        fetchQuestion();
+    }, []);
 
     return (
         <div className='w-4/5'>
+            <h1 className='text-3xl font-bold text-white mb-8'>{question}</h1>
             <Slider value={value} setValue={setValue} />
             <button onClick={submit} className='mt-5 bg-primary p-3 shadow-xl hover:bg-white'>
                 Send inn svar
